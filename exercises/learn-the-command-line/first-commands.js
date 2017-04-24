46d321c5754935e3bf908970f990ac9f585b29b8
Michael Bishop <polishthebishop@gmail.com>
2017-04-23 17:12:22 -0700

    starting through redirection

---
+++ exercises/learn-the-command-line/first-commands.js
@@ -0,0 +1,15 @@
+
+// First Commands
+
+
+1. ``` ls ```  // short for list
+
+2. ``` pwd ``` // print-working-directory
+
+3. ``` cd 2015 ``` ``` cd .. ```  // change directory / go up a directory
+
+4. ``` mkdir media ``` // makes directory called media
+
+5. ``` touch 2014/dec/keyboard.txt ``` // creates a file
+
+// COMMAND LINE QUIZ
---
+++ exercises/learn-the-command-line/redirection.js
@@ -0,0 +1,10 @@
+
+// Redirection
+
+//**1 standard input, abbreviated as stdin, is information inputted into the terminal through the keyboard or input device.
+//**2 standard output, abbreviated as stdout, is the information outputted after a process is run.
+//**3 standard error, abbreviated as stderr, is an error message outputted by a failed process.
+
+1. ``` echo "Hello" ``` // prints Hello to the console
+
+2. ``` echo "Hello" > hello.txt ``` ```cat hello.txt ```
