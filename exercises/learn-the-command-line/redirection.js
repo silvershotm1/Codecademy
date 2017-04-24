75d412323337b114c75dc3f77aa87716b7b21387
Michael Bishop <polishthebishop@gmail.com>
2017-04-24 11:41:29 -0700

    re-direction complete

--- exercises/learn-the-command-line/redirection.js
+++ exercises/learn-the-command-line/redirection.js
@@ -1,10 +1,29 @@
 
 // Redirection
 
-//**1 standard input, abbreviated as stdin, is information inputted into the terminal through the keyboard or input device.
-//**2 standard output, abbreviated as stdout, is the information outputted after a process is run.
-//**3 standard error, abbreviated as stderr, is an error message outputted by a failed process.
+//**1 stdin, is information inputted into the terminal through the keyboard or input device.
+//**2 stdout, is the information outputted after a process is run.
+//**3 stderr, is an error message outputted by a failed process.
 
 1. ``` echo "Hello" ``` // prints Hello to the console
 
 2. ``` echo "Hello" > hello.txt ``` ```cat hello.txt ```
+// takes Hello and writes it to file
+
+3. ```cat glaciers.txt >> rivers.txt```  // > overwrites while >> adds to file.
+
+4. ``` cat < lakes.txt ``` // takes std input from right and inputs to program on left.
+
+5.  ```cat volcanoes.txt | wc ``` // wordcount -- lines, words, characters
+
+6.  ``` sort lakes.txt ``` // sorts names in file.
+
+7. ``` sort deserts.txt | uniq > uniq-deserts.txt ``` // removes duplicates.
+
+8. ``` grep Mount mountains.txt ``` // looks for mount in file.
+
+9. ```grep -R Arctic /home/ccuser/workspace/geography ``` // Search Artic in folder
+
+10. ```sed 's/snow/rain/g' forests.txt ``` //
+// sed is stream editor  s = substitution;
+// snow = text to find; rain = replacement string; g = global
