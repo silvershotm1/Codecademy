3b60161bb9d1f250bf71e275e7ed4935983b72ee
Michael Bishop <polishthebishop@gmail.com>
2017-04-24 13:02:46 -0700

    branching complete

---
+++ exercises/learn-git/git-branching.js
@@ -0,0 +1,14 @@
+
+// Branching in Git
+
+1. ``` git branch ``` // checks what current branch you are on.
+
+2. ```git checkout -b fencing ``` // creates new branch fencing
+
+3. ```git checkout fencing ``` // switches to branch fencing
+
+4. ```git merge <branch-name> ``` // merges branch into current branch
+
+5. ```git merge fencing-- ERR ``` // this merge gives error which needs to be fixed
+
+6. ``` git branch -d <branch-name> ``` // this will delete the branch
