c0b594edf3c17d991bbcb0be41cc22c4b4ca2b02
Michael Bishop <polishthebishop@gmail.com>
2017-05-15 12:07:04 -0700

    started java

---
+++ exercises/learn-java/intro-java.java
@@ -0,0 +1,28 @@
+// 1. Intro
+public class YourName {
+	public static void main(String[] args) {
+
+		System.out.println("Michael");
+
+	}
+}
+// 2 DATA TYPES -- INTEGER
+public class DataTypes {
+	public static void main(String[] args) {
+
+		System.out.println(123385);
+
+	}
+}
+// 3 DATA TYPES -- BOOLEAN
+public class DataTypesB {
+	public static void main(String[] args) {
+
+		System.out.println(true);
+
+	}
+}
+
+
+// NOTES
+// 'int' data type only allows values between -2,147,483,648 and 2,147,483,647.
