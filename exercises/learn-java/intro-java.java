e4669de9239e226e19f53eab56c42bf9516447da
Michael Bishop <polishthebishop@gmail.com>
2017-05-15 12:18:26 -0700

    continue java

--- exercises/learn-java/intro-java.java
+++ exercises/learn-java/intro-java.java
@@ -6,7 +6,7 @@ public class YourName {
 
 	}
 }
-// 2 DATA TYPES -- INTEGER
+// 2. DATA TYPES -- INTEGER
 public class DataTypes {
 	public static void main(String[] args) {
 
@@ -14,7 +14,7 @@ public class DataTypes {
 
 	}
 }
-// 3 DATA TYPES -- BOOLEAN
+// 3. DATA TYPES -- BOOLEAN
 public class DataTypesB {
 	public static void main(String[] args) {
 
@@ -22,7 +22,68 @@ public class DataTypesB {
 
 	}
 }
+// 4. DATA TYPES -- CHARS
+public class DataTypesC {
+  public static void main(String[] args) {
+
+    System.out.println('C');
+
+  }
+}
+// 5. VARIABLES
+public class Variables {
+	public static void main(String[] args) {
+
+		int myNumber = 42;
+		boolean isFun = true;
+		char movieRating = 'A';
+
+	}
+}
+// 6. WHITESPACE
+public class WhiteSpace {
+	public static void main(String[] args) {
+
+		boolean isFormatted = false;
+		System.out.println(isFormatted);
+	}
+}
+// 7. COMMENTS
+public class Comments {
+	public static void main(String[] args) {
+
+		//System.out.println("Noise!");
+
+		/*
+
+    This is multi-line comment
+
+		*/
+
+
+	}
+}
+// 8. MATH + - * /
+public class Arithmetic {
+	public static void main(String[] args) {
+
+		int myNumber = 25 * 8;
+		System.out.println(myNumber);
+
+	}
+}
+// 9. MATH %(modulo)
+public class Modulo {
+	public static void main(String[] args) {
+
+		int myRemainder = 18 % 4;
+		System.out.println(myRemainder);
+
+	}
+}
 
 
 // NOTES
 // 'int' data type only allows values between -2,147,483,648 and 2,147,483,647.
+// 'boolean' set to true/false
+// 'char' must be indicated with a single quote
